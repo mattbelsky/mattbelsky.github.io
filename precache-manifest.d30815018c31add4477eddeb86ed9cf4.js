2f1665c019264e81552b6cca7687d7af63b43e63
Matthew <matt.belsky@gmail.com>
2019-10-07 19:59:08 -0400

    Updates

---
+++ precache-manifest.d30815018c31add4477eddeb86ed9cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cae4ed74cdcaeae5550fab2fc340008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c6da1d05813c4d4657d",
+    "url": "/static/css/main.f4be5512.chunk.css"
+  },
+  {
+    "revision": "a17558b599ddea22fed4",
+    "url": "/static/js/2.98d518e0.chunk.js"
+  },
+  {
+    "revision": "4c6da1d05813c4d4657d",
+    "url": "/static/js/main.e448fbd6.chunk.js"
+  },
+  {
+    "revision": "399390815bb9939ab7fe",
+    "url": "/static/js/runtime-main.898968dc.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import Root from './App';
-
-ReactDOM.render(<Root />, document.getElementById('root'));
-
